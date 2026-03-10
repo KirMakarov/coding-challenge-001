b241e97e3e778c087e44fd792fe222c2a3376977
KirMakarov <59772829+KirMakarov@users.noreply.github.com>
2026-03-10 23:12:15 +0100

    feat: add logger utility

---
+++ utils/logger.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
+
+def get_logger(name: str):
+    """Returns a logger instance with the specified name."""
+    return logging.getLogger(name)
