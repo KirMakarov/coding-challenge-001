3ff486cb011f68d5452684569ef7c39f6f98bb80
KirMakarov <59772829+KirMakarov@users.noreply.github.com>
2026-03-11 00:06:46 +0100

    feat: change main function: report generation and CSV export

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+
 from reconciliation import Reconciliation
 from utils.logger import get_logger
 
 logger = get_logger(__name__)
 
+
 def main():
     try:
+        output_path = Path.cwd() / "reconciliation_report.csv"
         reconciliation = Reconciliation()
-        reconciliation.generate_and_print_report()
+        report = reconciliation.generate_report()
+        report.print_report()
+        report.export_to_csv(output_path)
+
     except Exception as e:
         logger.error(f"An error occurred: {e}", exc_info=True)
 
